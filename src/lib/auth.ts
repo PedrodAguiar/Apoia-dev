d5f7b2aa6c5d95576688d5728232f82acc41a9d8
PedrodAguiar <pedrodaniel8416@gmail.com>
2025-07-29 02:13:16 -0300

    fit: ajustando login com github

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,8 @@ import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "./prisma"
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  secret: process.env.AUTH_SECRET!,
+  trustHost: true,
   providers: [GitHub],
   adapter: PrismaAdapter(prisma)
 })
